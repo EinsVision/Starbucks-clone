cdf64354c03dbe92b5754b3367fbd4a36dca3d9b
Changdae <einsvision@gmail.com>
2021-03-01 01:51:33 +0900

    logo to go home

--- starbucks-clone/src/App.js
+++ starbucks-clone/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Fade } from 'react-awesome-reveal';
 import HomeScreen from './screens/HomeScreen';
 import Header from './Header';
--- starbucks-clone/src/Header.js
+++ starbucks-clone/src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Header.css';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className='header'>
-      
+      <div className="header__left">
+        <Link className='header__logo' to='/'>
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" 
+            alt=""
+          />
+        </Link>
+
+        <Link className='header__link' to='/menu'>
+          Menu
+        </Link>
+
+      </div>
+
+      <div className="header__right">
+
+      </div>
     </div>
   )
 }
