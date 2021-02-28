b423070184599cf7be11ed9220117019eeec50d5
Changdae <einsvision@gmail.com>
2021-03-01 01:19:24 +0900

    made homepage structure

--- starbucks-clone/src/App.js
+++ starbucks-clone/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Fade } from 'react-awesome-reveal';
+import HomeScreen from './screens/HomeScreen';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -10,12 +14,12 @@ function App() {
         <Switch>
           <Route exact path="/">
             <h2>Starbucks Clone☕🍵🧉</h2>
-            {/* Header */}
+            <Header />
 
-            {/* HomeScreen */}
+            <HomeScreen />
 
-            {/* Fade */}
-              {/* Footer */}
+            <Fade />
+              <Footer />
           </Route>
         </Switch>
     </Router>
