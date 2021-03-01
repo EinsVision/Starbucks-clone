fd020d51238a5ba143b4548f80f3a36800e46451
Changdae <einsvision@gmail.com>
2021-03-01 13:52:01 +0900

    dropdown side right bar

--- starbucks-clone/src/MenuLink.js
+++ starbucks-clone/src/MenuLink.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './MenuLink.css';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { motion } from 'framer-motion';
 
-function MenuLink() {
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+export const  MenuLink = ({link, path, icon, goBackIcon, onClick, width}) => {
   return (
     <div>
-      
+      <Link to={path}>
+        <motion.li 
+          style={{ width}} 
+          onClick={onClick} 
+          variants={variants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {goBackIcon && <ChevronLeftIcon fontSize='large'/> }
+          {link}
+          {icon && <ChevronRightIcon fontSize='large'/>}
+        </motion.li>
+      </Link>
     </div>
   )
 }
