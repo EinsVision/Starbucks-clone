2725abb9a2872a5cffe8d3069b1b67767fa3a177
Changdae <einsvision@gmail.com>
2021-03-02 00:21:13 +0900

    building login page using react-hook form

--- starbucks-clone/src/screens/LoginScreen.js
+++ starbucks-clone/src/screens/LoginScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './LoginScreen.css';
+import { useForm } from "react-hook-form";
+import { TextField } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import ReportProblemIcon from '@material-ui/icons/ReportProblem';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 
 function LoginScreen() {
+
+  const { register, handleSubmit, watch, errors } = useForm();
+  const [passwordShown, setPasswordShown] = useState(false);
+  const onSubmit = ({ email, password }) => {
+
+  };
+  
   return (
     <div className='loginScreen'>
       <div className="loginScreen__left">
@@ -18,7 +31,67 @@ function LoginScreen() {
       </div>
 
       <div className="loginScreen__right">
-        <form></form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="loginScreen__inputContainer">
+            <TextField
+              label='Standard' 
+              name='email' 
+              type='email' 
+              InputLabelProps={{ style: { color: 'rgba(0,0,0,0.56)'}}} 
+              InputProps={{style: {fontWeight: '800' }}}
+              className='loginScreen__input'
+              inputRef={register({ required: true })}
+            /> 
+            {errors.email && (
+              <div className="loginScreen__error">
+                <CloseIcon fontSize='small' />
+                <span>Enter an email</span>
+                <ReportProblemIcon
+                  fontSize='small'
+                  className='loginScreen__reportIcon'
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="loginScreen__inputContainer">
+            <TextField
+              label='Standard' 
+              name='password' 
+              type='password' 
+              InputLabelProps={{ style: { color: 'rgba(0,0,0,0.56)'}}} 
+              InputProps={{style: {fontWeight: '800' }}}
+              className='loginScreen__input'
+              inputRef={register({ required: true })}
+            /> 
+            {passwordShown ? (
+              <VisibilityIcon
+                onClick={() => setPasswordShown(!passwordShown)}
+                className='loginScreen__visibilityIcon'
+              />
+            ) : (
+              <VisibilityOffIcon 
+                onClick={() => setPasswordShown(!passwordShown)}
+                className='loginScreen__visibilityIcon'
+              />
+            )}
+
+            {errors.password && (
+              <div className="loginScreen__error">
+                <CloseIcon fontSize='small' />
+                <span>Enter an password</span>
+                <ReportProblemIcon
+                  fontSize='small'
+                  className='loginScreen__reportIcon'
+                />
+              </div>
+            )}
+          </div>
+        </form>
+        <div className="loginScreen__resetLinks">
+          <Link>Forget your username?</Link>
+          <Link>Forget your password?</Link>
+        </div>
       </div>
     </div>
   )
