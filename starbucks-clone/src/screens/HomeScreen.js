5925ac6bfba52b84c3172502f1d5232c846627dc
Changdae <einsvision@gmail.com>
2021-03-01 15:17:28 +0900

    making home screen to show more information

--- starbucks-clone/src/screens/HomeScreen.js
+++ starbucks-clone/src/screens/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
 import { Link } from 'react-router-dom';
+import Featured from '../Featured';
 import Heading from '../Heading';
+import Info from '../Info';
 import './HomeScreen.css';
 
 
@@ -36,7 +38,56 @@ function HomeScreen() {
       </Fade>
 
       <Fade>
-        <Heading heading='TODAY IS YOURS' />
+        <Heading heading='MAKE TIME FOR ME TIME' />
+      </Fade>
+
+      <div className="homeScreen__featured">
+        <Fade>
+          <Featured
+            title='OH SO LUSH'
+            info='Enjoy velvety-smooth Nitro Cold Brew your way—black or with house-made Sweet Cream for under 75 calories in a grande.'
+            image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68963.png' 
+            background='#f0ebe0'
+            color='#1e3932'
+            className='featured__hoverLight'
+          />
+        </Fade>
+        
+        <Fade>
+          <Featured
+            title='UP AND AT ’EM'
+            info='Conquer it all with a Bacon, Sausage & Egg Wrap or Bacon, Gouda & Egg Sandwich, featuring cage-free eggs.'
+            image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68965.png' 
+            background='#f0ebe0'
+            color='#1e3932'
+            className='featured__hoverLight'
+          />
+        </Fade>
+      </div>
+
+      <Fade>
+        <Heading heading='MORE TO DISCOVER' />
+      </Fade>
+
+      <Fade>
+        <div className="homeScreen__discover">
+          <Info
+            title='STARBUCKS RESERVE® COFFEE'
+            info='Now for a limited time, you can explore these two incredible offerings: Ethiopia Yirgacheffe® Chelelektu and Guatemala Huehuetenango.'
+            image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68960.png' 
+            background='#f0ebe0'
+            color='#1e3932'
+            className='featured__hoverLight'
+          />
+          <Info
+            title='Earning 4,500 Bonus Stars is Super Starifying'
+            info='$0 intro annual fee for your first year with the Starbucks® Rewards Visa® Card. Plus your Stars won’t expire—an exclusive Starbucks benefit for cardmembers. Now that’s Super Starifying.**'
+            image='https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-63859.jpg' 
+            background='#f0ebe0'
+            color='#1e3932'
+            className='featured__hoverLight'
+          />
+        </div>
       </Fade>
     </div>
   )
