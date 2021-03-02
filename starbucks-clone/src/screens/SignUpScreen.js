0d22256ab154d32180e8bb3a1fb09b86773a9f97
Changdae <einsvision@gmail.com>
2021-03-02 12:05:38 +0900

    styling signup page

--- starbucks-clone/src/screens/SignUpScreen.js
+++ starbucks-clone/src/screens/SignUpScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FooterSecondary from '../FooterSecondary';
 import './SignUpScreen.css';
 
 function SignUpScreen() {
   return (
-    <div className='signupScreen'>
-      
+    <div className='signUpScreen'>
+      <div className="signUpScreen__header">
+        <Link to='/'>
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png" 
+            alt=""
+          />
+        </Link>
+      </div>
+      <h1 className='singUpScreen__heading'>Create an account</h1>
+      <div className="signUpScreen__rewards">
+        <h4>STARBUCKS REWARDS</h4>
+        <p>Join Starbucks Rewards to earn Stars for free food and drinks, any way you pay. Get access to mobile ordering, a birthday Reward, and <Link>more.</Link></p>
+      </div>
+      {/* <SignupForm /> */}
+      <FooterSecondary alignItems='center' flexDirection='column'/>
     </div>
   )
 }
