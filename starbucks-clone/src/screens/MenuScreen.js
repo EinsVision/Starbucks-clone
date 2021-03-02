4054d74116860d40e7adf1fc0dceb717042c34d3
Changdae <einsvision@gmail.com>
2021-03-02 17:34:48 +0900

    Done it, If we login, show the menu page

--- starbucks-clone/src/screens/MenuScreen.js
+++ starbucks-clone/src/screens/MenuScreen.js
@@ -1,10 +1,58 @@
 import React from 'react';
-
+import './MenuScreen.css';
+import menuList from '../menuList.json';
+import MenuItem from '../MenuItem';
 
 function MenuScreen() {
   return (
     <div className='menuScreen'>
-      <h1>I am the menu</h1>
+      {/* <MenuHeader /> */}
+      <div className="menuScreen__container">
+        <div className="menuScreen__left">
+
+        </div>
+
+        <div className="menuScreen__right">
+          <h1>Menu</h1>
+
+          <div className="menuScreen__category">
+            <h2>Drinks</h2>
+            <div className="menuScreen__items">
+              {menuList.map((menuListCategory) => menuListCategory.drinks.map(({ type, path, image}) => (
+                <MenuItem type={type} image={image} />
+              )))}
+            </div>
+          </div>
+
+          <div className="menuScreen__category">
+            <h2>Food</h2>
+            <div className="menuScreen__items">
+              {menuList.map((menuListCategory) => menuListCategory.food.map(({ type, path, image}) => (
+                <MenuItem type={type} image={image} />
+              )))}
+            </div>
+          </div>
+
+          <div className="menuScreen__category">
+            <h2>At home coffee</h2>
+            <div className="menuScreen__items">
+              {menuList.map((menuListCategory) => menuListCategory.atHomeCoffee.map(({ type, path, image}) => (
+                <MenuItem type={type} image={image} />
+              )))}
+            </div>
+          </div>
+
+          <div className="menuScreen__category">
+            <h2>Merchandise</h2>
+            <div className="menuScreen__items">
+              {menuList.map((menuListCategory) => menuListCategory.merchandise.map(({ type, path, image}) => (
+                <MenuItem type={type} image={image} />
+              )))}
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
