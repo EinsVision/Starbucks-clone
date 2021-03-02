c3ab2a810b5f9a1c2f278126c8d52350e43913c8
Changdae <einsvision@gmail.com>
2021-03-03 00:27:03 +0900

    log out redirection using useHistory

--- starbucks-clone/src/LogoutButton.js
+++ starbucks-clone/src/LogoutButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { logout } from './features/userSlice';
 import { auth } from './firebase';
 import './LogoutButton.css';
 
 function LogoutButton() {
-
+  const dispatch = useDispatch();
+  const history = useHistory();
   const logoutApp = () => {
-    auth.signOut();
+    auth.signOut().then(() => {
+      dispatch(logout())
+      history.replace('/');
+    })
+    .catch((error) => alert(error))
   };
 
   return (
