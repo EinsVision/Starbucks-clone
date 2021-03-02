e9b53399d11763abf1bde46628ce862eeccff3bd
Changdae <einsvision@gmail.com>
2021-03-02 10:58:37 +0900

    built footer second

--- starbucks-clone/src/FooterSecondary.js
+++ starbucks-clone/src/FooterSecondary.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './FooterSecondary.css';
+import FindStore from './FindStore';
+import { Link } from 'react-router-dom';
 
-function FooterSecondary() {
+function FooterSecondary({ alignItems, paddingLeft, flexDirection }) {
   return (
-    <div>
-      
+    <div 
+      className='footerSecondary' 
+      style={{alignItems, paddingLeft: `${paddingLeft*2}px`}}
+    >
+      <div 
+        className="footerSecondary__container" 
+        style={{ paddingLeft: `${paddingLeft}px`}}
+      >
+        <div className="footerSecondary__info">
+          <div className="footerSecondary__left">
+            <FindStore />
+          </div>
+          <div className="footerSecondary__right" style={{flexDirection}}>
+            <Link>Responsiblity</Link>
+            <Link>Web Accessibility</Link>
+            <Link>Privacy Policy</Link>
+            <Link>Terms of Use</Link>
+            <Link>Cookie Preferences</Link>
+          </div>
+        </div>
+        <span className='footerSecondary_copyright'>® 2021 Startbucks</span>
+      </div>  
     </div>
   )
 }
